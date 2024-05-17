09f9c11c80822353f589391398734dd90f0fceea
fit2cloudzhao <452988409@qq.com>
2024-05-17 15:37:00 +0800

    fix: 修改样式

--- src/views/login.vue
+++ src/views/login.vue
@@ -79,9 +79,9 @@
       </span>
     </el-dialog>
     <!--  底部  -->
-    <div class="el-login-footer">
-      <span>Copyright © 2017-2023 ichengle.top 技术支持：关注“编写美好前程”微信公众号，回复：支持</span>
-    </div>
+<!--    <div class="el-login-footer">-->
+<!--      <span>Copyright © 2017-2023 ichengle.top 技术支持：关注“编写美好前程”微信公众号，回复：支持</span>-->
+<!--    </div>-->
   </div>
 </template>
 
--- src/views/register.vue
+++ src/views/register.vue
@@ -60,9 +60,9 @@
       </el-form-item>
     </el-form>
     <!--  底部  -->
-    <div class="el-register-footer">
-      <span>Copyright © 2017-2023 ichengle.top 技术支持：关注“编写美好前程”微信公众号，回复：支持</span>
-    </div>
+<!--    <div class="el-register-footer">-->
+<!--      <span>Copyright © 2017-2023 ichengle.top 技术支持：关注“编写美好前程”微信公众号，回复：支持</span>-->
+<!--    </div>-->
   </div>
 </template>
 
